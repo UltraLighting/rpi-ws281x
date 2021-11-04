3f2e493d58379e5dfc268a60effc05384c0cce06
Phil <philip.schaefer0@gmail.com>
2021-11-05 00:55:20 +0100

    added Organge PI Zero

--- src/rpi_ws281x/rpihw.c
+++ src/rpi_ws281x/rpihw.c
@@ -459,6 +459,16 @@ static const rpi_hw_t rpi_hw_info[] = {
         .videocore_base = VIDEOCORE_BASE_RPI2,
         .desc = "Compute Module 3+",
     },
+    //
+    // Organge PI Zero
+    //
+    {
+        .hwver  = 0x0,
+        .type = RPI_HWVER_TYPE_PI2,
+        .periph_base = PERIPH_BASE_RPI2,
+        .videocore_base = VIDEOCORE_BASE_RPI2,
+        .desc = "Organge PI Zero",
+    },
 
 
 };
